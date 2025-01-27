d53d03f2ac715bd139acfcd289fc2aa5ace34249
Larissa <larissa.cavalcante014@gmail.com>
2025-01-27 20:34:52 -0300

    atualização da autenticação através do router

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,18 +2,41 @@ import {createRouter, createWebHashHistory} from 'vue-router';
 import HomeView from '@/view/HomeView.vue';
 import LoginPage from '@/view/LoginPage.vue';
 import MoviePage from '@/view/MoviePage.vue';
+import SeriesPage from '@/view/SeriesPage.vue';
 import RegisterPage from '@/view/RegisterPage.vue';
+import UserPage from '@/view/UserPage.vue';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
 
 const routes = [
-  { path: '/', component: HomeView },
+  { path: '/', component: HomeView},
   { path: '/login', component: LoginPage},
   { path: '/register', component: RegisterPage},
-  { path: '/movie', component: MoviePage}
+  { path: '/movie', component: MoviePage},
+  { path: '/series', component: SeriesPage},
+  { path: '/user', component: UserPage, meta: {requiresAuth: true}}
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes
 });
+
+router.beforeEach((to, from, next) =>{
+  const auth = getAuth();
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+  if (requiresAuth){
+    onAuthStateChanged(auth, (user) => {
+      if (user){
+        next();
+      }
+      else{
+        next('/login');
+      }
+    })
+  }
+  else{
+    next();
+  }
+})
 
 export default router;
--- src/view/UserPage.vue
+++ src/view/UserPage.vue
@@ -1,8 +1,7 @@
 <script setup>
 import HeaderView from '../components/fixed/HeaderView.vue';
-import MovieCard from '../components/navigation/component/ComponentCard.vue';
 
-//const series = [
+//const movies = [
 //];
 </script>
 
@@ -11,22 +10,10 @@ import MovieCard from '../components/navigation/component/ComponentCard.vue';
     <main>
       <div class="filters">
         <select>
-          <option value="">Filtrar por gênero</option>
+          <option value="">Filtrar</option>
           <option value="a-z">A/Z</option>
           <option value="z-a">Z/A</option>
-          <option value="action">Ação</option>
-          <option value="animation">Animação</option>
-          <option value="comedy">Comédia</option>
-          <option value="drama">Drama</option>
         </select>
-      </div>
-
-      <div class="series-grid">
-        <MovieCard 
-          v-for="movie in movies" 
-          :key="movie.id"
-          :movie="movie"
-        />
       </div>
   </main>
 </template>
@@ -43,7 +30,7 @@ import MovieCard from '../components/navigation/component/ComponentCard.vue';
   border-radius: 4px;
 }
 
-.series-grid {
+.movies-grid {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
   gap: 2rem;
@@ -62,7 +49,7 @@ import MovieCard from '../components/navigation/component/ComponentCard.vue';
     width: 100%;
   }
 
-  .series-grid {  
+  .movies-grid {  
     grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
   }
 }
