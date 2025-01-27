e2a05aba05e6cffec562df538198177708acaa84
Larissa <larissa.cavalcante014@gmail.com>
2025-01-27 20:50:44 -0300

    implementação do LoginService

---
+++ src/services/LoginService.js
@@ -0,0 +1,17 @@
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+class LoginService{
+    constructor(){
+        this.auth = getAuth();
+    };
+
+    async login(email, password){
+        try{
+            const userCredential = await signInWithEmailAndPassword(this.auth, email, password);
+            return userCredential.user;
+        }catch{
+            console.log('Erro ao realizar login: ', error.message);
+            throw error;
+        };
+    };
+};
